5d6978b4b238a132cf47ec76194a1b4d95921164
bgocap <diego.capcha98@hotmail.com>
2023-09-22 18:10:38 +0300

    Exercise 5.16

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom'
 import { fireEvent, render, screen } from '@testing-library/react'
 import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
+import NewBlogForm from './NewBlogForm'
 
 test('renders initial content', () => {
     const blog = {
@@ -66,4 +67,29 @@ test('props are being recieved twice', async () => {
 
     expect(mockHandler.mock.calls).toHaveLength(2)
 
+})
+
+test('Check form details of the eventHandler when new blog is created', async () =>{
+
+    const newBlog ={
+        title:"Yundt, Hackett and Trantow",
+        author:"Liza Daspar",
+        url:"usnews.com",
+    }
+
+    const mockHandler = jest.fn()
+    const user = userEvent.setup()
+
+    const {container} = render (< NewBlogForm submitBlog={mockHandler}/>)
+    const submitButton = screen.getByText('save')
+    
+    await user.type(container.querySelector('[name="Title"]'),newBlog.title) 
+    await user.type(container.querySelector('[name="Author"]'),newBlog.author)
+    await user.type(container.querySelector('[name="URL"]'),newBlog.url)
+    
+    await user.click(submitButton)
+
+    screen.debug()
+
+    expect(mockHandler.mock.calls[0][0]).toEqual(newBlog)
 })
